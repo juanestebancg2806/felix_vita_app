b9d244f2e021c877b10ada00aaefb81dcd8f8070
Juan Esteban Cardona <juan.garcia@bld.ai>
2024-06-06 17:54:32 -0500

    configure initial backend project in Java Springboot

---
+++ backend/felix-app/src/main/java/com/felix_vita_app/felix_app/FelixAppApplication.java
@@ -0,0 +1,13 @@
+package com.felix_vita_app.felix_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FelixAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FelixAppApplication.class, args);
+	}
+
+}
---
+++ backend/felix-app/src/test/java/com/felix_vita_app/felix_app/FelixAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felix_vita_app.felix_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FelixAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
